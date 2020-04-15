1fc7561e7d57d0cc6c0b1421629e61e7a708322d
AutomicDemoCAmena <61249931+AutomicDemoCAmena@users.noreply.github.com>
2020-04-15 14:33:44 +0400

    Terraform Example01 for AWS

---
+++ AWS/Example01/main.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+//variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
+variable "aws_ami" {default = "ami-0003569dd0860582f"}
+variable "aws_security_group_id" {default = "sg-d1375eb1"}
+variable "instance_type" {default = "t2.micro"}
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_instance" "cda_instance" {
+  ami                    = "${var.aws_ami}"
+  instance_type          = "${var.instance_type}"
+  vpc_security_group_ids = ["${var.aws_security_group_id}"]
+  key_name	             = "jeny-key-us-east-1"
+}
