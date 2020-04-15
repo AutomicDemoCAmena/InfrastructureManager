59328da82a6c63f280c13915311ee3e2165a18ba
AutomicDemoCAmena <61249931+AutomicDemoCAmena@users.noreply.github.com>
2020-04-15 17:18:09 +0400

    added key

--- AWS/main.tf
+++ AWS/main.tf
@@ -4,17 +4,8 @@ variable "aws_secret_key" {}
 variable "aws_ami" {default = "ami-0003569dd0860582f"}
 variable "aws_security_group_id" {default = "sg-d1375eb1"}
 variable "instance_type" {default = "t2.micro"}
-variable "key_name" {default = "automic_aws_demo_key"}
+variable "key_name" {default = "automic_demo_key_pem"}
 
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = "${var.key_name}"
-  public_key = "${tls_private_key.example.public_key_openssh}"
-}
 provider "aws" {
   region     = "us-east-2"
   access_key = "${var.aws_access_key}"
